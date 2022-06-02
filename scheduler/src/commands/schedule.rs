10afdb32a4aa71e964eec38fe12760cc15290d67
Sven Hemmecke <43651925+svenh27@users.noreply.github.com>
2022-06-02 21:52:48 +0200

    fixed some mistakes

--- scheduler/src/commands/schedule.rs
+++ scheduler/src/commands/schedule.rs
@@ -21,15 +21,19 @@ pub fn schedule(_schedule: Schedule) -> Result<()> {
             (0..N).map(|j| {
                 if i<=j {false} //obere Dreicksmatrix
                 else if i==0 || i==N-1 {true} //Anfangs/Endpunkt
-                else {rand::thread_rng().gen_range(0..3)==0} //chance is 1/3 here for edges
+                else {
+                    let t :i32=rand::thread_rng().gen_range(0..3);
+                    if t==0{true}
+                    else {false}
+                } //chance is 1/3 here for edges
             }).collect()
         }).collect();
     //number of ressources
     let M = rand::thread_rng().gen_range(3..7);
     //maximal resources of system
-    let Rmax :Vec<i32> = (0..M).map(|x| rand::thread_rng().gen_range(6..14)).collect();
+    let Rmax :Vec<i32> = (0..M).map(|_| rand::thread_rng().gen_range(6..14)).collect();
     //ressource matrix
-    let R :Vec<Vec<i32>> = (0..N).map(|i| {
+    let R :Vec<Vec<i32>> = (0..N).map(|_| {
             (0..M).map(|j| {
                 //makes lower values where one process wont need more then
                 // half the ressources of one type more likely, so sheduling makes more sense
@@ -40,77 +44,95 @@ pub fn schedule(_schedule: Schedule) -> Result<()> {
         }).collect();
     //============ End Definition of Problem ============
     let delta=rand::thread_rng().gen_range(3..5);
-    let mut Nred: Vec<(i32, i32)> = Vec::new();
+    let mut Nred: Vec<(usize, usize)> = Vec::new();
     for swap_range in 1..delta+1{
-        Nred.append((0..N-swap_range).map(|i| (i,i+swap_range)).collect());
+        let mut temp : Vec<(usize, usize)>=(0 as usize..(N-swap_range) as usize).map(|i| (i,i+swap_range as usize)).collect();
+        Nred.append(&mut temp);
     }
 
 
     let InitialSolutions :Vec<Vec<i32>> = vec![(0..N).collect()];
     //because Definition is like that, but there are more initials (4.1.)
     Algorithm1(&Nred, &InitialSolutions[0], &E);
+
+    Ok(())
 }
 
-fn Algorithm1(Nred: &Vec<(i32, i32)>, sched: &Vec<i32>, edges:&Vec<Vec<bool>>) -> Vec<(i32, i32)> {
-    let mut moves=Nred.clone();
-    /*
+fn Algorithm1(Nred: &Vec<(usize, usize)>, sched: &Vec<i32>, edges:&Vec<Vec<bool>>) -> Vec<(usize, usize)> {
+    let mut moves: Vec<(usize, usize)>=Nred.clone();
+    let moves_len=moves.len();
+    //*
     //removes unfeasible(5)
-    for i in 0..moves.len(){
+    for i in 0..moves_len{
         let u=moves[i].0;
         let v=moves[i].1;
         for x in u+1 .. v+1{
-            if edges[sched[u]][sched[x]] {moves[i]=(0,0);break;}
+            if edges
+                [sched[u] as usize]
+                [sched[x as usize] as usize]
+            {moves[i]=(0,0);break;}
         }
     }
     //moves (0,0) to the end
-    let i=0;
-    invalid=0;
-    while i<moves.len()-invalid{
+    let mut i=0;
+    let mut invalid=0;
+    while i<moves_len-invalid{
         if moves[i]==(0,0){
-            while moves[moves.len()-invalid-1]==(0,0){
+            while moves[moves_len-invalid-1]==(0,0){
                 invalid+=1;
             }
-            if moves.len()-invalid-1<=i {break;}
-            moves.swap(i,moves.len()-invalid-1);
+            if moves_len-invalid-1<=i {break;}
+            moves.swap(i,moves_len-invalid-1);
             invalid+=1;
         }
+        i+=1;
     }
     //removes unfeasible(6)
-    for i in 0..moves.len()-invalid{
+    for i in 0..moves_len-invalid{
         let u=moves[i].0;
         let v=moves[i].1;
         for x in u .. v{
-            if edges[sched[x]][sched[v]] {moves[i]=(0,0);break;}
+            if edges
+                [sched[x] as usize]
+                [sched[v] as usize]
+            {moves[i]=(0,0);break;}
         }
     }
     //moves (0,0) to the end
-    let i=0;
+    i=0;
     // invalid=0; //no reset for invalids in the end
-    while i<moves.len()-invalid{
+    while i<moves_len-invalid{
         if moves[i]==(0,0){
-            while moves[moves.len()-invalid-1]==(0,0){
+            while moves[moves_len-invalid-1]==(0,0){
                 invalid+=1;
             }
-            if moves.len()-invalid-1<=i {break;}
-            moves.swap(i,moves.len()-invalid-1);
+            if moves_len-invalid-1<=i {break;}
+            moves.swap(i,moves_len-invalid-1);
             invalid+=1;
         }
+        i+=1;
     }
-    */
-    //*
-    moves.iter().filter(|(u,v)| {
+    //*/
+    /*
+    moves=moves.into_iter().filter(|(u,v)| {
         for x in u+1 .. v+1{
-            if edges[sched[u]][sched[x]] {return false}
+            if edges
+                [sched[u.clone()] as usize]
+                [sched[x] as usize]
+            {return false}
         }
         true
-    });
-    moves.iter().filter(|(u,v)| {
-        for x in u .. v{
-            if edges[sched[x]][sched[v]] {return false}
+    }).collect();
+    moves=moves.into_iter().filter(|(u,v)| {
+        for x in u.clone() .. v.clone(){
+            if edges
+                [sched[x] as usize]
+                [sched[v.clone()] as usize]
+            {return false}
         }
         true
-    });
-
+    }).collect();
+    //let t = sched.get(5);
     // */
     return moves
 }
