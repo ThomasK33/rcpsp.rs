efeec200a158a03c3830ebaa7961282941443f23
Thomas Kosiewski <thoma471@googlemail.com>
2022-06-30 00:36:03 +0200

    Reduced sampling size to 10

--- rcpsp/benches/scheduler.rs
+++ rcpsp/benches/scheduler.rs
@@ -9,19 +9,21 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         let contents = std::fs::read_to_string(format!("../examples/{file}")).unwrap();
         let psp = parse_psp_lib(contents.as_str()).unwrap();
 
-        group.bench_function(BenchmarkId::new("simple_schedule", file), |b| {
-            b.iter(|| {
-                simple_schedule(
-                    psp.clone(),
-                    rcpsp::schedulers::simple::SimpleScheduleOptions {
-                        number_of_iterations: 200,
-                        max_iter_since_best: 20,
-                        tabu_list_size: 15,
-                        swap_range: 10,
-                    },
-                )
-            })
-        });
+        group
+            .sample_size(10)
+            .bench_function(BenchmarkId::new("simple_schedule", file), |b| {
+                b.iter(|| {
+                    simple_schedule(
+                        psp.clone(),
+                        rcpsp::schedulers::simple::SimpleScheduleOptions {
+                            number_of_iterations: 200,
+                            max_iter_since_best: 20,
+                            tabu_list_size: 15,
+                            swap_range: 10,
+                        },
+                    )
+                })
+            });
     }
     group.finish();
 }
